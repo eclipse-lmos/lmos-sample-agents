f7ce814699eb3520d1576eab00ff3522d56c0785
Patrick Schwager <patrick.schwager@telekom.de>
2025-03-13 16:46:37 +0100

    revert release changes

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+
+plugins {
+    id("net.researchgate.release") version "3.1.0"
+}
+
+release {
+    newVersionCommitMessage = "New Snapshot-Version:"
+    preTagCommitMessage = "Release:"
+}
--- productsearch-agent/build.gradle.kts
+++ productsearch-agent/build.gradle.kts
@@ -4,10 +4,4 @@
 
 plugins {
     id("multimodule-config")
-    id("net.researchgate.release") version "3.1.0"
-}
-
-release {
-    newVersionCommitMessage = "New Snapshot-Version:"
-    preTagCommitMessage = "Release:"
 }
